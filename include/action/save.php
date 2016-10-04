8dfebe54d9747b9d35eacb91a5e5610c79cab6bd
Tangui Morlier <tangui@tangui.eu.org>
2016-10-04 11:55:19 +0200

    Bon lien

--- include/action/save.php
+++ include/action/save.php
@@ -42,7 +42,7 @@ $doc = get_document_from_id($_SESSION['document_id']);
 $req = $bdd->prepare('UPDATE documents SET ips = :ips, tries = :tries WHERE id = :document_id');
 $req->execute(array('ips' => $doc['ips'].','.$_SERVER['REMOTE_ADDR'].',', 'tries' => $doc['tries'] + 1, 'document_id' => $_SESSION['document_id']));
 
-$_SESSION['lastpage'] = "./contribution.php?id=".$_SESSION['document_id'];
+$_SESSION['lastpage'] = "./consultation.php?id=".$_SESSION['document_id'];
 $_SESSION['sent_ok'] = true;
 $_SESSION['token'] = null;
 $_SESSION['document_id'] = null;

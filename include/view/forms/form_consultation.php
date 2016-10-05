df0422e4c4ab3c9bd982eac147195d61005734ab
Tangui Morlier <tangui@tangui.eu.org>
2016-10-05 13:52:47 +0200

    Présentations des affirmations + suppression de Ignorer

--- include/view/forms/form_consultation.php
+++ include/view/forms/form_consultation.php
@@ -6,13 +6,25 @@
     <?php endforeach; ?>
     <label class="btn btn-default" id="autresthemes">Autre</label>
   </div>
-  <div class="form-group">
-    <label>Quel usage trouveriez vous pertinant de réaliser avec cette contribution&nbsp;?</label>
-    <div class="text-center"><div class="btn-group" data-toggle="buttons">
-      <label class="btn btn-default"><input type="radio" name="realisation|l'ignorer" id="option1" autocomplete="off">l'ignorer</label>
-      <label class="btn btn-default"><input type="radio" name="realisation|en faire un usage statistique" id="option2" autocomplete="off">en faire un usage statistique</label>
-      <label class="btn btn-default"><input type="radio" name="realisation|la synthétiser" id="option3" autocomplete="off">la synthétiser</label>
-      <label class="btn btn-default"><input type="radio" name="realisation|la publier" id="option4" autocomplete="off">la publier</label>
-      <label class="btn btn-default"><input type="radio" name="realisation|mérite une audition" id="option5" autocomplete="off">mérite une audition</label>
-    </div></div>
+</div>
+<div class="form-group">
+  <label>La contribution contient&nbsp;:</label>
+  <div class="row">
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|l'évocation d'expérience(s) personnelle(s)">l'évocation d'expérience(s) personnelle(s)</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des informations ou données sur le problème">des informations ou données sur le problème</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une analyse des causes du problème">une analyse des causes du problème</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des prises de position contre une mesure existante">des prises de position contre une mesure existante</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des propositions de nouvelles mesures">des propositions de nouvelles mesures</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des questions ou demandes d'information">des questions ou demandes d'information</label></div>
+    <div class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des prises de position en faveur d'une mesure existante">des prises de position en faveur d'une mesure existante</label></div>
   </div>
+</div>
+<div class="form-group">
+  <label>Quel usage trouveriez vous pertinant de réaliser avec cette contribution&nbsp;?</label>
+  <div class="text-center"><div class="btn-group" data-toggle="buttons">
+    <label class="btn btn-default"><input type="radio" name="realisation|en faire un usage statistique" id="option2" autocomplete="off">en faire un usage statistique</label>
+    <label class="btn btn-default"><input type="radio" name="realisation|la synthétiser" id="option3" autocomplete="off">la synthétiser</label>
+    <label class="btn btn-default"><input type="radio" name="realisation|la publier" id="option4" autocomplete="off">la publier</label>
+    <label class="btn btn-default"><input type="radio" name="realisation|mérite une audition" id="option5" autocomplete="off">mérite une audition</label>
+  </div></div>
+</div>

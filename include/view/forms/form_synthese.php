10f8d6ba9fdf93c585f7725c6ee9862648e1392f
Tangui Morlier <tangui@tangui.eu.org>
2016-10-05 14:18:53 +0200

    Ajout d'un formulaire de synthèse

---
+++ include/view/forms/form_synthese.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+  <label>Vous avez indiqué que la constribution contient « l'expression d'une expérience personnelle ». Pouvez vous synthétiser cet élément en 100 caractères (ou moins) :</label>
+  <textarea class="form-control" rows="5" id="synthese" maxlength="100"></textarea>
+</div>

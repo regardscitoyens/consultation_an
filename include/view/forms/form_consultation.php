0cef239847bd5739af18910826507dbe28b4514a
Tangui Morlier <tangui@tangui.eu.org>
2016-10-07 16:42:22 +0200

    Présentation

--- include/view/forms/form_consultation.php
+++ include/view/forms/form_consultation.php
@@ -3,22 +3,22 @@
   <p class="text-muted">Pour mieux traiter les constributions, nous avons besoin de les classer par thèmes.</p>
   <div id="themes" class="row">
     <?php foreach (get_document_categories() as $c ) : ?>
-        <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" autocomplete="off" name="themes|<?php echo $c; ?>"><?php echo $c; ?></label></div>
+        <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-4"><label><input type="checkbox" autocomplete="off" name="themes|<?php echo $c; ?>"><?php echo $c; ?></label></div>
     <?php endforeach; ?>
     </div>
-    <a id=autresthemes href="">Ajouter un theme</a>
+    <p class="text-right"><a id=autresthemes href="">Ajouter un theme</a></p>
 </div>
 <div class="form-group">
   <label>La contribution contient&nbsp;:</label>
   <p class="text-muted">Les contributions peuvent contenir des éléments très différents. Adiez nous à les identifier.</p>
   <div class="row">
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|l'évocation d'expérience(s) personnelle(s)" value="l'évocation d'expérience(s) personnelle(s)">L'évocation d'expérience(s) personnelle(s)</label></div>
-    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une information ou donnée sur le problème" value="des informations ou données sur le problème">Des informations ou données sur le problème</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une analyse des causes du problème" value="une analyse des causes du problème">Une analyse des causes du problème</label></div>
+    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une information ou donnée sur le problème" value="des informations ou données sur le problème">Des informations ou données sur le problème</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une prise de position contre une mesure existante" value="des prises de position contre une mesure existante">Des prises de position contre une mesure existante</label></div>
+    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une prise de position en faveur d'une mesure existante" value="des prises de position en faveur d'une mesure existante">Des prises de position en faveur d'une mesure existante</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une proposition de nouvelles mesures" value="des propositions de nouvelles mesures">Des propositions de nouvelles mesures</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une question ou demande d'information" value="des questions ou demandes d'information">Des questions ou demandes d'information</label></div>
-    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une prise de position en faveur d'une mesure existante" value="des prises de position en faveur d'une mesure existante">Des prises de position en faveur d'une mesure existante</label></div>
   </div>
 </div>
 <div class="form-group">

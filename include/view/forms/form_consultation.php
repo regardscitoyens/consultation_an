1a4bdaf8e0b5e6c74c77ec7dc6186f1f708a3f5e
RouxRC <b.ooghe@gmail.com>
2016-10-10 02:13:46 +0200

    typos + list todo

--- include/view/forms/form_consultation.php
+++ include/view/forms/form_consultation.php
@@ -5,7 +5,7 @@
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|l'évocation d'expérience(s) personnelle(s)" value="l'évocation d'expérience(s) personnelle(s)">L'évocation d'expérience(s) personnelle(s)</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une analyse des causes du problème" value="une analyse des causes du problème">Une analyse des causes du problème</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une description d'un problème" value="une description d'un problème">Une description d'un problème <small>(exemples, données…)</small></label></div>
-    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des analyses ou proposition d'ordre général" value="des analyses ou proposition d'ordre général">Des analyses ou proposition d'ordre général</label></div>
+    <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|des analyses ou proposition d'ordre général" value="des analyses ou proposition d'ordre général">Des analyses ou propositions d'ordre général</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une proposition de nouvelles mesures" value="des propositions de nouvelles mesures">Des propositions de nouvelles mesures</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une question ou demande d'information" value="des questions ou demandes d'information">Des questions ou demandes d'information</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="checkbox col-md-6"><label><input type="checkbox" name="affirmations|une prise de position en faveur d'une mesure existante" value="des prises de position en faveur d'une mesure existante">Des prises de position en faveur d'une mesure existante</label></div>
@@ -14,17 +14,17 @@
 </div>
 <div class="form-group">
   <label>Avez-vous déjà entendu un tel propos dans le débat public&nbsp;?</label>
-  <div class="text-muted">Aidez nous à faire émerger les contributions les plus originales.</div>
+  <div class="text-muted">Aidez-nous à faire émerger les contributions les plus originales.</div>
   <div class="row">
     <div style="margin-top: 2px; margin-bottom: 2px;" class="radio col-md-1"><label><input type="radio" name="original|non">Oui</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="radio col-md-1"><label><input type="radio" name="original|oui">Non</label></div>
   </div>
 </div>
 <div class="form-group">
-  <label>Si vous étiez députés, que feriez vous de cette contribution&nbsp;?</label>
-  <p class="text-muted">Aidez nous à faire émerger les propos les plus intéressants en vous mettant dans la peau d'un député.</p>
+  <label>Si vous étiez député, que feriez-vous de cette contribution&nbsp;?</label>
+  <p class="text-muted">Aidez-nous à faire émerger les propos les plus intéressants en vous mettant dans la peau d'un député.</p>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|en faire un usage statistique" id="option2">Je reçois personnellement son auteur</label></div>
     <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|la synthétiser" id="option3">Je cite les propos dans mon rapport</label></div>
-    <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|la publier" id="option4">Je prend en compte les propos dans mon travail</label></div>
-    <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|mérite une audition" id="option5">Rien (Je demande à mes équipes de remercier son auteur pour sa contribution)</label></div>
+    <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|la publier" id="option4">Je prends en compte les propos dans mon travail</label></div>
+    <div style="margin-top: 2px; margin-bottom: 2px;" class="radio"><label><input type="radio" name="realisation|mérite une audition" id="option5">Rien (je demande à mes équipes de remercier son auteur pour sa contribution)</label></div>
 </div>

da629de9aca4447895a0f0332656ac9b76cceddd
François Massot <francois.massot@gmail.com>
2016-10-17 23:22:10 +0200

    Remove parlementaire related scripts

--- bin/download_consultation_data.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p data
-for i in 2 3 4 5 6 ; do
-  curl "http://data.assemblee-nationale.fr/static/openData/repository/CONSULTATIONS_CITOYENNES/EGALITE/EGALITE"$i".json" | sed 's/\({"id":"[^"]*","sexe"\)/\n\1/g' | sed 's/.$//' > "data/EGALITE"$i".json"
-done
